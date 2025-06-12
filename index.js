c4049036cf9ce4ed9c1d4c01aa3efe3c943a8455
Sefinek <contact@sefinek.net>
2025-06-12 14:24:22 +0200

    4kb

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express();
 
 // Middlewares
 app.use(helmet());
-app.use(express.json({ limit: '6kb' }));
+app.use(express.json({ limit: '4kb' }));
 app.use(logger);
 app.use(limiter);
 app.use(timeout());
