f61d99ce270f870aaf6de91800d8b8b1a67831e7
Sefinek <contact@sefinek.net>
2025-07-13 14:38:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('env-native').config();
 
 const express = require('express');
 const { networkInterfaces } = require('node:os');
-const { version } = require('./package.json');
+const { name, version } = require('./package.json');
 
 // Middleware imports
 const helmet = require('helmet');
@@ -40,7 +40,7 @@ app.use(internalError);
 // Listen
 app.listen(port, () => {
 	const localLinks = getLocalNetworkLinks();
-	console.log(`[${process.env.NODE_ENV.toUpperCase()}] Waiting for events (version v${version})`);
+	console.log(`[${process.env.NODE_ENV.toUpperCase()}] Waiting for events (${name}/${version})`);
 	console.log(`- Local  : http://127.0.0.1:${port}/discord/webhook`);
 	localLinks.forEach(url => console.log(`- Network: ${url}/discord/webhook`));
 
